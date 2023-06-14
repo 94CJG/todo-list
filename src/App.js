95090d99ce37f5d546fa9be49e6382262f1a3539
94CJG <krsoi9412@gmail.com>
2023-06-15 01:23:18 +0900

    user input 코드 수정 및 index.js 내용 수정
    App.js내용
    ㅁ user이름 입력후 자기이름이 화면에 출력 되고, input + button 사라지는 코드로 수정
    ㅁ 코드를 작성 했을 때 잘 몰라서 chat.gpt 도움을 받음
    ㅁ 조건부 랜더링을 사용하여 구현을 할 수있다는 것을 알게됨. (그 전에도 알고 있었지만 기억을 못함...)
    ㅁ 코드에 대해서 부족한 부분 그리고 const명에 대해서 피드백좀 부탁 드립니다!
    
    index.js내용
    ㅁ main 커밋 한 이력을 봤을 때 같은 현상
    ㅁ 동일하게 작업함
    
    브랜치 충돌현상 발생
    ㅁ 브랜치에서 작업을 하고 push를 했는데 충돌이 일어남
    ㅁ gpt를 통해서 방법을 터득하고 문제를 해결할 수 있었음
    ㅁ git pull --rebase origin username을 사용 하였고
    ㅁ 충돌 반영이된 코드들 수정 한 뒤 git add src/App.js ->  git rebase --continue를 통해 수정을 완료함

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-//username으로 브런치 생성
 //git pull로 다시 받아와서 재실행
 //2023.06.14 재시작
 import { useState } from 'react';
 
 function App() {
+  // todo-list
   const [todo, setTodo] = useState('');
   const [list, setList] = useState([]);
+
+  //user
+  const [userName, setUserName] = useState('');
+  const [userSave, setUserSave] = useState([]);
+
+  // todo-list
   const onChange = (e) => setTodo(e.target.value);
   const onSubmit = (e) => {
     e.preventDefault();
@@ -24,15 +30,40 @@ function App() {
     );
   };
 
+  // user
+  const userOnChange = (e) => setUserName(e.target.value);
+  const userOnSubmit = (e) => {
+    e.preventDefault();
+    if (userName === '') {
+      return;
+    }
+    setUserSave((addUser) => [userName, ...addUser]);
+    setUserName('');
+  };
+  console.log(userSave);
+
   return (
     <div>
       <h1>Todo-list Practice !!</h1>
-      <form>
+      {/* 사용자 이름 */}
+      {userSave.length === 0 ? (
+        <form onSubmit={userOnSubmit}>
+          <div>
+            <input
+              id="name"
+              value={userName}
+              onChange={userOnChange}
+              type="text"
+              placeholder="write your name!"
+            />
+            <button>Add User Name</button>
+          </div>
+        </form>
+      ) : (
         <div>
-          <input id="name" type="text" placeholder="write your name!" />
-          <button>Add User Name</button>
+          <h2>User Name: {userSave[0]}</h2>
         </div>
-      </form>
+      )}
 
       {/* ToDolist 작성하는 곳 */}
       <div>
