d598e27277f958a2dc4c555ac123dccda90b0be2
94CJG <krsoi9412@gmail.com>
2023-06-18 21:02:27 +0900

    마우스 움직임 'Mouse.module.css' 파일 추가, 사용자 && ToDo-list 코드 수정
    ㅁ리엑트 공식 문서를 보는중, 현재 App에 기능을 추가 해보고 싶어서
    마우스 움직일때 감지하는 'pointermove' 이벤트리스너 추가.
    ㅁ86번째와 87번째줄 코드가 중복 작성이 됐는데 변수로 담아서 사용 할 수 있을꺼 같지만 적용 방법을 모름.
    ㅁcss 모듈 파일을 추가 했지만, 93번째줄에 있는 코드를 css파일에 어떻게 추가 해야할 지를 모름.
    ㅁ수정한 코드들은 친구와 gpt를 통해서 물어보면서 코드수정 진행.
    ㅁtodo-list에 내용을 입력시 로컬스토리지에는 데이터가 저장이 됐지만, 새로고침을 하면 화면에 리스트들이 사라짐.
    ㅁ주석 해놓았던 todo-list에서  useEffect를 if문을 추가하여 조건이 false일때 함수를 종료 하고,
    JSON함수를 변수에 담아서 코드의 가시성을 수정함.
    ㅁtodos를 의존성배열을 추가하여 todos가 변경시 실행.
    ㅁ'userSave' -> 'users'로 변수를 수정 했고, index번호를 이용하여 0 번째만 받아 올 수 있게 코드 수정
    ㅁ reat-dev 문서를 보긴 했으나.. 말이 어려움.... 어떻게 이해하고 방식을 적용 해야할지 잘 모르겠음.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
+//새로고침시 로컬스토리지에 바로 추가 안됨
+//새로고침시 제일 최근에 저장해뒀던 list가 사라짐
+
 //css 추가
 import styles from './Button.module.css';
 import style from './Reset.module.css';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import mouse from './Mouse.module.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  // mosue
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
   // todo-list
   const [todo, setTodo] = useState('');
-  const [list, setList] = useState([]);
+  const [todos, setToDos] = useState([]);
 
   //user
   const [userName, setUserName] = useState('');
-  const [userSave, setUserSave] = useState([]);
+  const [users, setUsers] = useState([]);
 
   // todo-list
   const onChange = (e) => setTodo(e.target.value);
@@ -20,22 +26,31 @@ function App() {
     if (todo === '') {
       return;
     }
-    setList((addList) => [todo, ...addList]);
-    localStorage.setItem('list', JSON.stringify(list));
+    setToDos((addToDos) => [todo, ...addToDos]);
     setTodo('');
   };
+
+  useEffect(() => {
+    if (todos.length === 0) return;
+    const jsonStr = JSON.stringify(todos);
+    localStorage.setItem('todos', jsonStr);
+  }, [todos]);
+
+  useEffect(() => {
+    const localToDos = localStorage.getItem('todos');
+    if (!localToDos) return;
+    const storedToDos = JSON.parse(localToDos);
+    setToDos(storedToDos || []);
+  }, []);
+
   const onClick = (e) => {
     const delteBtn = e.target.id;
-    setList((addList) =>
-      addList.filter((item) => {
+    setToDos((addToDos) =>
+      addToDos.filter((item) => {
         return item !== delteBtn;
       })
     );
   };
-  useEffect(() => {
-    const storedList = JSON.parse(localStorage.getItem('list'));
-    setList(storedList || []);
-  }, []);
 
   // user
   const userOnChange = (e) => setUserName(e.target.value);
@@ -44,16 +59,44 @@ function App() {
     if (userName === '') {
       return;
     }
-    setUserSave((addUser) => [userName, ...addUser]);
+    setUsers((addUser) => [userName, ...addUser]);
+    setUserName(userName);
     setUserName('');
+    console.log(users);
   };
-  console.log(list);
+
+  useEffect(() => {
+    if (users.length === 0) return;
+    const jsonSetUsers = JSON.stringify(users);
+    localStorage.setItem('Users', jsonSetUsers);
+  }, [users]);
+
+  useEffect(() => {
+    const localGetUsers = localStorage.getItem('Users');
+    if (!localGetUsers) return;
+    const localParseUsers = JSON.parse(localGetUsers);
+    setUsers(localParseUsers || []);
+  }, []);
+
+  // mouseMove
+  useEffect(() => {
+    function handleMove(e) {
+      setPosition({ x: e.clientX, y: e.clientY });
+    }
+    window.addEventListener('pointermove', handleMove);
+    return () => window.addEventListener('pointermove', handleMove);
+  }, []);
 
   return (
     <div className={style.div}>
+      <div
+        className={mouse.pink}
+        style={{ transform: `translate(${position.x}px, ${position.y}px)` }}
+      ></div>
+
       <h1>Todo-list Practice !!</h1>
       {/* 사용자 이름 */}
-      {userSave.length === 0 ? (
+      {users.length === 0 ? (
         <form onSubmit={userOnSubmit}>
           <div>
             <input
@@ -68,27 +111,27 @@ function App() {
         </form>
       ) : (
         <div>
-          <h2 className={styles.userName}>User Name: {userSave[0]}</h2>
+          <h2 className={styles.userName}>User Name: {users[0]}</h2>
         </div>
       )}
 
-      {/* ToDolist 작성하는 곳 */}
+      {/* ToDotodos 작성하는 곳 */}
       <div>
-        <h2>add ToDo-List</h2>
+        <h2>add ToDo-todos</h2>
         <form onSubmit={onSubmit}>
           <input
             onChange={onChange}
             value={todo}
             className={styles.botBtn}
-            id="list"
+            id="todos"
             type="text"
-            placeholder="write your toDolist"
+            placeholder="write your todo-list"
           />
           <button>Add To Do</button>
         </form>
       </div>
       <ul>
-        {list.map((item, index) => (
+        {todos.map((item, index) => (
           <li key={index} className={styles.li}>
             {item}
             <button id={item} onClick={onClick}>
@@ -104,9 +147,7 @@ function App() {
 export default App;
 
 /** 질문
- * chatGpt를 사용해서 useEffect를 사용 코드 적용함
-
- * JSON.stringfy() 와 JSON.parse()의 차이점 및 사용처?
- * useEffect를 왜 사용하여 새로고침시 초기화가 안될까?
- * 로컬스토리지에 확인시 저장 값이 바로 안보인다.
+ * 현재 93번째줄에 style추가 한 내용을 css쪽에다 어떻게 지정을 해줄수있을까?
  */
+
+// * 공식문서 읽다가 마우스 이벤트리스너 추가
